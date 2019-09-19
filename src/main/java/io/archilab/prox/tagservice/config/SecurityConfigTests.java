a91c30273e19d9ed1de223d874d0b42bdc14fb22
Stephan <44501469+stephanhhhh@users.noreply.github.com>
2019-09-19 20:25:40 +0200

    test big data profile

--- src/main/java/io/archilab/prox/tagservice/config/SecurityConfigTests.java
+++ src/main/java/io/archilab/prox/tagservice/config/SecurityConfigTests.java
@@ -17,9 +17,9 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Profile("!disabled-security")
+@Profile("disabled-security")
 @KeycloakConfiguration
-class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+class SecurityConfigTests extends KeycloakWebSecurityConfigurerAdapter {
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -51,11 +51,6 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     super.configure(http);
     http.csrf().disable().authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/tags/**").permitAll()
-        .antMatchers("/tags/**").hasRole("professor")
-        .antMatchers(HttpMethod.GET, "/tagCollections/**").permitAll()
-        .antMatchers("/tagCollections/**").hasRole("professor")
-        .antMatchers(HttpMethod.GET, "/tagRecommendations/**").permitAll()
-        .anyRequest().denyAll();
+        .anyRequest().permitAll();
   }
 }
