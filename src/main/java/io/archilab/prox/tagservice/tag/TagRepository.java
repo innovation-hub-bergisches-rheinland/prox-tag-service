40c70df629f5d383e7c4cdd083fa3d87327a46af
JannIntveen <JannIntveen@gmail.com>
2019-09-23 13:11:28 +0200

    find by tag name now returns set

--- src/main/java/io/archilab/prox/tagservice/tag/TagRepository.java
+++ src/main/java/io/archilab/prox/tagservice/tag/TagRepository.java
@@ -1,16 +1,15 @@
 package io.archilab.prox.tagservice.tag;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Set;
+import java.util.UUID;
 
 
 public interface TagRepository extends PagingAndSortingRepository<Tag, UUID> {
 
-  Optional<Tag> findByTagName_TagName(@Param(value = "tagName") String tagName);
+  Set<Tag> findByTagName_TagName(@Param(value = "tagName") String tagName);
 
   Set<Tag> findByTagName_TagNameContaining(@Param(value = "tagName") String tagName);
 
