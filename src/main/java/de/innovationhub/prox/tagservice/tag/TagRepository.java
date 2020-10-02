306878e66f4fbaa06f3be52df36118813abf1610
Tobias Hund <tobiashund6112@gmail.com>
2020-10-02 08:44:49 +0200

    Fix PROX-85: Tag search should ignore case

--- src/main/java/de/innovationhub/prox/tagservice/tag/TagRepository.java
+++ src/main/java/de/innovationhub/prox/tagservice/tag/TagRepository.java
@@ -28,10 +28,13 @@ import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface TagRepository extends PagingAndSortingRepository<Tag, UUID>, TagRepositoryCustom {
 
-  Set<Tag> findByTagName_TagName(@Param(value = "tagName") String tagName);
+  @RestResource(path = "findByTagName_TagName")
+  Set<Tag> findByTagNameTagNameIgnoreCase(@Param(value = "tagName") String tagName);
 
-  Set<Tag> findByTagName_TagNameContaining(@Param(value = "tagName") String tagName);
+  @RestResource(path = "findByTagName_TagNameContaining")
+  Set<Tag> findByTagNameTagNameContainingIgnoreCase(@Param(value = "tagName") String tagName);
 }
