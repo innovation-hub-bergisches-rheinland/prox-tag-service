4685b02fff037f07102cdb7adf30c770298256e0
Tobias Hund <tobias.hund@innovation-hub.de>
2020-11-27 16:58:52 +0100

    Add Security Context and Scheme

--- src/main/java/de/innovationhub/prox/tagservice/config/SpringfoxConfig.java
+++ src/main/java/de/innovationhub/prox/tagservice/config/SpringfoxConfig.java
@@ -1,16 +1,27 @@
 package de.innovationhub.prox.tagservice.config;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import org.apache.commons.lang.ArrayUtils;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.HttpAuthenticationScheme;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.OperationContext;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -20,10 +31,32 @@ public class SpringfoxConfig {
   public Docket api() {
     return new Docket(DocumentationType.OAS_30)
         .forCodeGeneration(true)
+        .securityContexts(Collections.singletonList(securityContext()))
+        .securitySchemes(Collections.singletonList(jwtScheme()))
         .select()
         .paths(PathSelectors.ant("/tags/**")
             .or(PathSelectors.ant("/tagCollections/**")))
         .apis(customRequestHandlers())
+        .build();
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(List.of(defaultAuth()))
+        .operationSelector(o -> o.httpMethod() != HttpMethod.GET)
+        .build();
+  }
+
+  private SecurityReference defaultAuth() {
+    return SecurityReference.builder()
+        .scopes(new AuthorizationScope[0])
+        .reference("JWT")
+        .build();
+  }
+
+  private SecurityScheme jwtScheme() {
+    return HttpAuthenticationScheme.JWT_BEARER_BUILDER
+        .name("JWT")
         .build();
   }
 
