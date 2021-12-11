9fe1f3c266c9c3ab7075945485e872efc694725a
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-12-11 16:19:26 +0100

    build: add maven updater

---
+++ .versionrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: 'pom.xml',
+      updater: './node_modules/standard-version-maven/index.js'
+    },
+    {
+      filename: 'package.json',
+      type: 'json'
+    }
+  ]
+};
