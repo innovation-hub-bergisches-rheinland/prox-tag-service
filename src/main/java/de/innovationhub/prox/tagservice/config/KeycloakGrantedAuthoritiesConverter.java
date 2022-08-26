d20c8f0e60c8bce793a80f9afe7ee047c845e558
Tobias Hund <tobias.hund@innovation-hub.de>
2022-08-26 22:29:18 +0200

    refactor: replace keycloak dependency with oauth2 resource server

---
+++ src/main/java/de/innovationhub/prox/tagservice/config/KeycloakGrantedAuthoritiesConverter.java
@@ -0,0 +1,38 @@
+package de.innovationhub.prox.tagservice.config;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class KeycloakGrantedAuthoritiesConverter
+  implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+  @Override
+  public Collection<GrantedAuthority> convert(Jwt source) {
+    Map<String, Object> realmAccess = source.getClaimAsMap("realm_access");
+
+    var roles = (List<String>) realmAccess.get("roles");
+    Stream<GrantedAuthority> roleAuthorities = Stream.empty();
+    if (roles != null) {
+      roleAuthorities = roles.stream()
+        .map(rn -> new SimpleGrantedAuthority("ROLE_" + rn));
+    }
+
+
+    var features = source.getClaimAsStringList("features");
+    Stream<GrantedAuthority> featureAuthorities = Stream.empty();
+    if(features != null) {
+      featureAuthorities = features.stream()
+        .map(fn -> new SimpleGrantedAuthority("FEATURE_" + fn));
+    }
+
+
+    return Stream.concat(roleAuthorities, featureAuthorities).collect(Collectors.toList());
+  }
+}
--- src/main/java/de/innovationhub/prox/tagservice/config/SecurityConfig.java
+++ src/main/java/de/innovationhub/prox/tagservice/config/SecurityConfig.java
@@ -2,65 +2,66 @@ package de.innovationhub.prox.tagservice.config;
 
 
 import java.util.List;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.web.cors.CorsConfiguration;
 
-@KeycloakConfiguration
-class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+@Configuration
+class SecurityConfig {
 
   public static final String[] SWAGGER_PATHS = {
     "/swagger-resources/**", "/swagger-ui/**", "/swagger-ui/", "/v2/api-docs", "/v3/api-docs"
   };
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    KeycloakAuthenticationProvider keycloakAuthenticationProvider =
-        this.keycloakAuthenticationProvider();
-    keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-    auth.authenticationProvider(keycloakAuthenticationProvider);
+  private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+    jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakGrantedAuthoritiesConverter());
+    jwtConverter.setPrincipalClaimName("sub");
+    return jwtConverter;
   }
 
   @Bean
-  @Override
-  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-    return new NullAuthenticatedSessionStrategy();
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    corsConfiguration.setAllowedHeaders(List.of("*"));
     corsConfiguration.setAllowedOrigins(List.of("*"));
-    corsConfiguration.setAllowedMethods(List.of("GET", "PUT", "OPTIONS"));
-    corsConfiguration.setExposedHeaders(List.of("Authorization"));
+    corsConfiguration.setAllowedMethods(List.of("*"));
 
     http.cors()
-        .configurationSource(request -> corsConfiguration)
-        .and()
-        .csrf()
-        .disable()
-        .authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/tags/**")
-        .permitAll()
-        .antMatchers(HttpMethod.HEAD, "/tags/**")
-        .permitAll()
-        .antMatchers(HttpMethod.OPTIONS, "/tags/**")
-        .permitAll()
-        .antMatchers("/tags/**")
-        .hasRole("professor")
-        .antMatchers(HttpMethod.GET, SWAGGER_PATHS)
-        .permitAll()
-        .anyRequest()
-        .denyAll();
+      .configurationSource(request -> corsConfiguration)
+      .and()
+      .csrf()
+      .disable()
+      .oauth2ResourceServer(
+        oauth2 -> oauth2.jwt().jwtAuthenticationConverter(jwtAuthenticationConverter()))
+      .authorizeHttpRequests(
+        registry ->
+          registry
+            .mvcMatchers(HttpMethod.GET, SWAGGER_PATHS)
+            .permitAll()
+            .mvcMatchers(HttpMethod.HEAD, SWAGGER_PATHS)
+            .permitAll()
+            .mvcMatchers(HttpMethod.OPTIONS, SWAGGER_PATHS)
+            .permitAll()
+            .mvcMatchers(HttpMethod.GET, "/tags/**")
+            .permitAll()
+            .mvcMatchers(HttpMethod.HEAD, "/tags/**")
+            .permitAll()
+            .mvcMatchers(HttpMethod.OPTIONS, "/tags/**")
+            .permitAll()
+            .mvcMatchers("/tags/**")
+            .hasRole("professor")
+            .anyRequest()
+            .denyAll());
+
+    return http.build();
   }
 }
